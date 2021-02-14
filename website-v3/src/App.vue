d3a019402a415a87fa57b754e20ac61a099e781b
nek <nastya9@icloud.com>
2021-02-15 01:20:19 +0300

    фикс модальных окон

--- website-v3/src/App.vue
+++ website-v3/src/App.vue
@@ -18,93 +18,13 @@
                 <!-- mobile proto -->
                 <div class="mobile-logo-container"><div class="logoNameMobile">Weight<br>Calendar</div></div>
                 <div class="logoName">WeightCalendar</div>
-
-
-                <div class="right-menu-buttons">
-                        <a class="login-block transition" href="#" id="modalOpen" ><div class="login">Войти</div></a>
-                        <a class="registration-block transition" href="#" id="modalOpen-registration"><div class="register" >Регистрация</div></a>
-                </div>
+                <rightMenuButtons></rightMenuButtons>
             </div>
         </header>
         <h1 class="firstMotivateText">Отслеживайте свой вес!</h1>
         <h2 class="secondMotivateText">Соблюдаете диету или занимаетесь спортом? WeightCalendar идеально подойдет для вас!</h2>
         <img src="./img/sketchpng.png" class="mainPng">
 
-        <div class="modalWin" id="modalid">
-              <div class="modalWindow">
-
-                <div class="modalHeader">
-                    <h2 class="modalHeader__text">Добро<br>Пожаловать</h2>
-                </div>
-                <div class="modalBody">
-                    <form action="" class="login-form" >
-
-                        <div class="login-form-centered">
-
-                            <p class="fields label-font m-b-forms">
-                                <label class="email-label" for="signin-email"></label>
-                                <input class="email-input label-form outline-off" id="signin-email" type="email" placeholder="Почта">
-
-                            </p>
-
-                            <p class="fields label-font m-b-forms">
-                                <label class="password-label " for="signin-password"></label>
-                                <input class="password-input label-form outline-off" id="signin-password" type="password" placeholder="Пароль">
-                            </p>
-                        </div>
-                        <div class="log-in-forgotten">
-                            <p class="fields label-font -margin-not">
-                                <input class="login-submit outline-off " type="submit" value="Войти">
-                            </p>
-                            <a href="#" class="forgotten label-font">Забыли пароль?</a>
-                        </div>
-                        <div class="registration-switch">
-                            <p href="#" class="notation-h label-font">Если у вас нет аккаунта, тогда Вам нужно</p>
-                            <a class="switch-button" href="#"><div class="registration-switch-div" ><p class="registration-switch-text">Зарегистрироваться</p></div></a>
-                        </div>
-                      </form>
-                </div>
-              </div>
-        </div>
-        <div class="modalWinReg" id="modalid-registration">
-            <div class="modalWindowReg">
-
-                <div class="modalHeaderReg">
-                    <h2 class="modalHeader__textReg">Регистрация</h2>
-                </div>
-                <div class="modalBodyReg">
-                    <form action="" class="registrationForm">
-                        <div class="name-surname-quter">
-                            <p class="fields m-b-forms">
-                                <label class="name-label" for="signup-name"></label>
-                                <input class="name-input regForm-fonts label-quter-form outline-off" id="signup-name" type="text" placeholder="Имя">
-                            </p>
-                            <p class="fields m-b-forms">
-                                <label class="surname-label" for="signup-surname"></label>
-                                <input class="surname-input regForm-fonts label-quter-form outline-off" id="signup-surname" type="text" placeholder="Фамилия">
-                            </p>
-                        </div>
-                        <div class="Reg-column-forms">
-                            <p class="fields label-font m-b-forms">
-                                <label class="email-label" for="signin-email"></label>
-                                <input class="email-input label-form outline-off" id="signin-email" type="email" placeholder="Почта">
-
-                            </p>
-                            <p class="fields label-font m-b-forms">
-                                <label class="password-label " for="signin-password"></label>
-                                <input class="password-input label-form outline-off" id="signin-password" type="password" placeholder="Пароль">
-                            </p>
-                            <p class="fields label-font m-b-forms">
-                                <label class="password-label " for="signin-password"></label>
-                                <input class="password-input label-form outline-off" id="signin-password" type="password" placeholder="Повторите пароль">
-                            </p>
-
-                            <input class="registration-submit outline-off label-form " type="submit" value="Зарегистрироваться">
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
         <div class="overlay" id="overlay-modal"></div>
         <mobileLogin></mobileLogin>
     </body>
@@ -112,14 +32,17 @@
 
 <script>
 import mobileLogin from './components/HelloWorld.vue'
+import rightMenuButtons from './components/rightMenuButtons.vue'
 
 export default {
   name: 'App',
   components: {
-    mobileLogin
+    mobileLogin,
+    rightMenuButtons
   }
 }
 </script>
+
 
 <style>
 @import "./css/forms.css";
@@ -127,4 +50,8 @@ export default {
 @import "./css/modal.css";
 @import "./css/reset.css";
 @import "./css/style.css";
+@import "https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css";
+/*@import "https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js";*/
+@import "https://fonts.googleapis.com/css2?family=Montserrat:wght@900&display=swap";
+@import "./fonts/SFPro/stylesheet.css";
 </style>
---
+++ website-v3/src/components/rightMenuButtons.vue
@@ -0,0 +1,113 @@
+<template>
+  <div class="right-menu-buttons">
+          <a class="login-block transition" href="#" id="modalOpen" ><div class="login" @click="appearLog = true">Войти</div></a>
+          <a class="registration-block transition" href="#" id="modalOpen-registration" ><div class="register" @click="appearReg = true">Регистрация</div></a>
+  </div>
+  <div class="modalWin" id="modalid" :style="appearLog ? showMod : hideMod" @click="hideLogin()">
+        <div class="modalWindow" @click="outside = false">
+
+          <div class="modalHeader">
+              <h2 class="modalHeader__text">Добро<br>Пожаловать</h2>
+          </div>
+          <div class="modalBody">
+              <form action="" class="login-form" >
+
+                  <div class="login-form-centered">
+
+                      <p class="fields label-font m-b-forms">
+                          <label class="email-label" for="signin-email"></label>
+                          <input class="email-input label-form outline-off" id="signin-email" type="email" placeholder="Почта">
+
+                      </p>
+
+                      <p class="fields label-font m-b-forms">
+                          <label class="password-label " for="signin-password"></label>
+                          <input class="password-input label-form outline-off" id="signin-password" type="password" placeholder="Пароль">
+                      </p>
+                  </div>
+                  <div class="log-in-forgotten">
+                      <p class="fields label-font -margin-not">
+                          <input class="login-submit outline-off " type="submit" value="Войти">
+                      </p>
+                      <a href="#" class="forgotten label-font">Забыли пароль?</a>
+                  </div>
+                  <div class="registration-switch">
+                      <p href="#" class="notation-h label-font">Если у вас нет аккаунта, тогда Вам нужно</p>
+                      <a class="switch-button" href="#"><div class="registration-switch-div" ><p class="registration-switch-text">Зарегистрироваться</p></div></a>
+                  </div>
+                </form>
+          </div>
+        </div>
+  </div>
+  <div class="modalWinReg" id="modalid-registration" :style="appearReg ? showMod : hideMod" @click="hideReg()">
+      <div class="modalWindowReg" @click="outside = false">
+
+          <div class="modalHeaderReg">
+              <h2 class="modalHeader__textReg">Регистрация</h2>
+          </div>
+          <div class="modalBodyReg">
+              <form action="" class="registrationForm">
+                  <div class="name-surname-quter">
+                      <p class="fields m-b-forms">
+                          <label class="name-label" for="signup-name"></label>
+                          <input class="name-input regForm-fonts label-quter-form outline-off" id="signup-name" type="text" placeholder="Имя">
+                      </p>
+                      <p class="fields m-b-forms">
+                          <label class="surname-label" for="signup-surname"></label>
+                          <input class="surname-input regForm-fonts label-quter-form outline-off" id="signup-surname" type="text" placeholder="Фамилия">
+                      </p>
+                  </div>
+                  <div class="Reg-column-forms">
+                      <p class="fields label-font m-b-forms">
+                          <label class="email-label" for="signin-email"></label>
+                          <input class="email-input label-form outline-off" id="signin-email" type="email" placeholder="Почта">
+
+                      </p>
+                      <p class="fields label-font m-b-forms">
+                          <label class="password-label " for="signin-password"></label>
+                          <input class="password-input label-form outline-off" id="signin-password" type="password" placeholder="Пароль">
+                      </p>
+                      <p class="fields label-font m-b-forms">
+                          <label class="password-label " for="signin-password"></label>
+                          <input class="password-input label-form outline-off" id="signin-password" type="password" placeholder="Повторите пароль">
+                      </p>
+
+                      <input class="registration-submit outline-off label-form " type="submit" value="Зарегистрироваться">
+                  </div>
+              </form>
+          </div>
+      </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'rightMenuButtons',
+  data: function () {
+    return{
+      showMod: "display: block",
+      hideMod: "display: none",
+      appearLog: false,
+      appearReg: false,
+      close: false
+    }
+  },
+  methods: {
+    hideLogin(){
+      if (this.outside) {
+        this.appearLog = false
+      }
+      this.outside = true;
+    },
+    hideReg(){
+      if (this.outside) {
+        this.appearReg = false
+      }
+      this.outside = true;
+    }
+  }
+}
+
+
+</script>
