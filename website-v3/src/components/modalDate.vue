751330e0d95c8412fc3c79983ed1ad54fd53c6c8
mappited <killer-killa89@mail.ru>
2021-03-15 23:48:15 +0300

    choose date form

--- website-v3/src/components/modalDate.vue
+++ website-v3/src/components/modalDate.vue
@@ -2,19 +2,64 @@
   <div class="modalWin" id="modalid" :style="appearDate ? showMod : hideMod" @click="hideDate()">
         <div class="modalWindow" @click="outside = false">
 
-          <div class="modalHeader" style="display:flex;">
-              <div class="fixationDate-button">
-                    <a href="#"  onclick="openbox('box1'); return false" class="fixationDate-button-link active-link-delStyles">Заданные интервалы</a>
+          <div class="modalHeader" style="display:flex; justify-content: space-between;">
+              
+                <a href="#"  @click="openbox('box1')" class="fixationDate-button-link active-link-delStyles">Заданные интервалы</a>
+                
+
+                
+                <a href="#" @click="openbox('box2')" class="chooseDate-button-link active-link-delStyles">Ввести дату</a>
+          </div>
+          <div class="modalBody">
+            <div class="content-body">
+                <div class="chooseContent-block" id="box1" style="display: none;">
+                  <div class="show-chosenBlock1"></div>
+
+                  <div class="contentOf-Blocks" >
+                    <h2 class="Blocks-Text">Выберите предустановленные<br>периоды</h2>
+                    <div class="BlockFixation-div">
+                      <form action="/profile" name="date">
+                        <div class="fixationList">
+                            <select name="name" id="" class="fixationSelect">
+                              <option value="1" class="Select-OP" selected>1 неделя</option>
+                              <option value="2" class="Select-OP">2 недели</option>
+                              <option value="1" class="Select-OP">1 месяц</option>
+                              <option value="6" class="Select-OP">6 месяцев</option>
+                              <option value="1" class="Select-OP">1 год</option>
+                            </select>
+                            <input type="submit" value="Выбрать" class="fixationSubmit-button outline-off">
+                          </div>
+                      </form>
+                    </div>
+                  </div>
+          
                 </div>
 
-                <div class="chooseDate-onCalendar">
-                    <a href="#" onclick="openbox('box2'); return false" class="chooseDate-buyyon-link active-link-delStyles">Ввести дату</a>
+
+
+
+
+
+                <div class="chooseContent-block" id="box2" style="display: none;">
+                  <div class="show-chosenBlock2"></div>
+                    <div class="contentOf-Blocks" >
+                    <h2 class="Blocks-Text">Выберите даты начала<br>и конца периода</h2>
+                    <div class="BlockFixation-div">
+                      <form action="/profile" name="date">
+                        <div class="fixationList">
+                          <div class="calendars-block">
+                              <div class="flexCalendars-block"><label for="start" class="labelForm-date">Начало измерений<input type="date" name="date" id="start" required class="styleDate-input"></label></div>
+
+                              <div class="flexCalendars-block"><label for="end" class="labelForm-date">Конец измерений<input type="date" name="date" id="end" required class="styleDate-input"></label></div>
+                          </div>
+                          <input type="submit" value="Выбрать" class="fixationSubmit-button outline-off">
+                        </div>
+                      </form>
+                    </div>
+                  </div>
+
+
                 </div>
-          </div>
-          <div class="modalBody" >
-            <div class="content-body">
-                <div class="chooseContent-block" id="box1" style="display: none;">1</div>
-                <div class="chooseContent-block" id="box2" style="display: none;">2</div>
             </div>
     
             
@@ -44,7 +89,21 @@ export default {
     },
     showDate(){
       this.appearDate = true
+    },
+    openbox(id){
+    let all = document.querySelectorAll(".chooseContent-block");
+    for (let i = 0; i < all.length; i++) {
+      if (all[i].id == id) {
+        all[i].style.display = (all[i].style.display == 'none')? 'block' : 'none';
+       
+      } else {
+        all[i].style.display = 'none';
+       
+      }
     }
+    
+   
+    },
   }
 }
 </script>
